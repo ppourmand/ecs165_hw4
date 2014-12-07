03d59ed8840381c8cacce2417555640547271573
Pasha Pourmand <pasha.pourmand@gmail.com>
2014-12-06 17:18:53 -0800

    can now read in csv properly, also outputs to standard out as a testing feature

--- loader.py
+++ loader.py
@@ -4,9 +4,19 @@
 #2014-12-6
 
 import psycopg2
+import csv
 
+# tries to connect to data base
 try:
     conn = psycopg2.connect("dbname='test' user='ppourmand' host='localhost'")
     print "I connected to the database successfully"
 except:
     print "I am unable to connect to the database"
+
+# open the csv file and read it in
+f = open('EIA_CO2_Electric_2014.csv')
+csv_f = csv.reader(f)
+
+# prints outs the csv to standard out
+for row in csv_f:
+	print row

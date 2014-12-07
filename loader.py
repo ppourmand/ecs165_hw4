252d7a20ce6308e61f791821f50004b22e4416ea
Pasha Pourmand <pasha.pourmand@gmail.com>
2014-12-06 17:22:39 -0800

    read in the other 2 EIA CSV files

--- loader.py
+++ loader.py
@@ -13,10 +13,16 @@ try:
 except:
     print "I am unable to connect to the database"
 
-# open the csv file and read it in
-f = open('EIA_CO2_Electric_2014.csv')
-csv_f = csv.reader(f)
+# open the co2 electric csv file and read it in
+EIA_CO2_Electric = csv.reader(open('EIA_CO2_Electric_2014.csv'))
+
+# open the co2 transportation csv file and read it in
+EIA_CO2_Transportation = csv.reader(open('EIA_CO2_Transportation_2014.csv'))
+
+# open the MkWh csv file and read it in
+EIA_MkWh = csv.reader(open('EIA_MkWh_2014.csv'))
+
 
 # prints outs the csv to standard out
-for row in csv_f:
+for row in EIA_CO2_Electric:
 	print row
